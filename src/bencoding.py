1c4c1e71fe5a76a7c8d616a376903b6c0bc8b72c
Bernard <bernardmonagan@gmail.com>
2025-09-22 21:39:45 -0400

    Small comment about doing the bencoding by hand in the future.

--- src/bencoding.py
+++ src/bencoding.py
@@ -1,4 +1,6 @@
 import bencodepy
+# Would be interesting to do code these manually in the future.
+# There are a few different libraries but it would make sense to have something of my own for it.
 
 def encode(data):
     return bencodepy.encode(data)
