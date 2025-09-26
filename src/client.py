73916c674cb3b61388cdd00f01b2d752a9c6a5c5
Bernard <bernardmonagan@gmail.com>
2025-09-26 00:25:55 -0400

    Working on Piece class and functions in client.py

--- src/client.py
+++ src/client.py
@@ -133,3 +133,55 @@ class Block:
         self.length = length
         self.status = Block.Missing
         self.data = None
+
+class Piece:
+    """
+    The piece is a part of of the torrents content. Each piece except the final
+    piece for a torrent has the same length (the final piece might be shorter).
+
+    A piece is what is defined in the torrent meta-data. However, when sharing
+    data between peers a smaller unit is used - this smaller piece is refereed
+    to as `Block` by the unofficial specification (the official specification
+    uses piece for this one as well, which is slightly confusing).
+    """
+
+    def __init__(self, index: int, block: [], hash_value):
+        self.index = index
+        self.blocks = blocks
+        self.hash = hash_value
+    
+    def reset(self):
+        """
+        Reset all blocks to missing regarless of current state
+        """
+        for block in self.blocks:
+            block.status = Block.Missing
+    
+    def next_request(self) -> Block:
+        """
+        Get the next Block to be requested
+        """
+        missing = [b for b in self.blocks if b.status is Block.Missing]
+        if missing:
+            missing[0].status = Block.Pending
+            return missing[0]
+        return None
+    
+    def block_received(self, offset: int, data: bytes):
+        """
+        Update block information that the given block is now received
+
+        :param offset: The block offset (within the piece)
+        :param data: The block data
+        """
+        matches = [b for b in self.blocks if b.offset == offset]
+        block = matches[0] if matches else None
+        if block:
+            block.status = Block.Retrieved
+            block.data = data
+        else:
+            logging.warning('Trying to complete a non-existing block {offset}'
+                            .format(offset=offset))
+    
+
+        
