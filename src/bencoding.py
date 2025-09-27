eaa1b0e610d3589e2bec6f2e9a0d1fc7d86f18fc
Bernard <bernardmonagan@gmail.com>
2025-09-27 00:16:55 -0400

    Fixed local_bencoding import and moved the functions out of the __init__ part of the class.

--- src/bencoding.py
+++
@@ -1,9 +0,0 @@
-import bencodepy
-# Would be interesting to do code these manually in the future.
-# There are a few different libraries but it would make sense to have something of my own for it.
-
-def encode(data):
-    return bencodepy.encode(data)
-
-def decode(data):
-    return bencodepy.decode(data)
--- src/torrent.py
+++ src/torrent.py
@@ -1,7 +1,7 @@
 from hashlib import sha1
 from collections import namedtuple
 
-from bencoding import encode, decode
+from .local_bencoding import encode, decode
 
 #Reprsents the files within the torrent
 TorrentFile = namedtuple('TorrentFile',['name','length'])
@@ -13,78 +13,78 @@ class Torrent:
 
         with open(self.filename,'rb') as f:
             meta_info = f.read()
-            self.meta_info = bencoding.decode(meta_info).decode()
-            info = bencoding.encode(self.meta_info[b'info']).encode()
+            self.meta_info = decode(meta_info)
+            info = encode(self.meta_info[b'info'])
             #TODO research if sha 1 is still the correct choice for something like this.
             self.info_hash = sha1(info).digest()
             self._identify_files()
         
 
-        def _identify_files(self):
-            """
-            identifies the files included in this torrent
-            """
+    def _identify_files(self):
+        """
+        identifies the files included in this torrent
+        """
 
-            if self.multi_file:
-                #TODO add support for multi-file torrents
-                raise RuntimeError('Multi-file torrents is not supported!')
-            self.files.append(
-                TorrentFile(
-                    self.meta_info[b'info'][b'name'].decode('utf-8'),
-                    self.meta_info[b'info'][b'length']))
-        
-        @property
-        def announce(self) -> str:
-            """
-            Announces URL to tracker
-            """
-            return self.meta_info[b'announce'].decode('utf-8')
-        
-        @property
-        def multi_file(self) -> bool:
-            """
-            Checks if torrent contains multiple files
-            """
-            return b'files' in self.meta_info[b'info']
-        
-        @property
-        def piece_length(self) -> int:
-            """
-            Gets the length in bytes for each piece of download
-            """
-            return self.meta_info[b'info'][b'piece length']
-        
-        @property
-        def total_size(self) -> int:
-            """
-            :return: The total size (in bytes) for this torrent's data.
-            """
-            if self.multi_file:
-                raise RuntimeError('Multi-file torrents are not supported!')
-            return self.files[0].length
-        
-        @property
-        def pieces(self) -> list[str]:
-            """
-            Breaks the string meta_info pieces into 20 byte long slices. 
-            20 bytes being 20 characters in a string representation
-            """
-            data = self.meta_info[b'info'][b'pieces']
-            pieces = []
-            offset = 0
-            length = len(data)
+        if self.multi_file:
+            #TODO add support for multi-file torrents
+            raise RuntimeError('Multi-file torrents is not supported!')
+        self.files.append(
+            TorrentFile(
+                self.meta_info[b'info'][b'name'].decode('utf-8'),
+                self.meta_info[b'info'][b'length']))
+    
+    @property
+    def announce(self) -> str:
+        """
+        Announces URL to tracker
+        """
+        return self.meta_info[b'announce'].decode('utf-8')
+    
+    @property
+    def multi_file(self) -> bool:
+        """
+        Checks if torrent contains multiple files
+        """
+        return b'files' in self.meta_info[b'info']
+    
+    @property
+    def piece_length(self) -> int:
+        """
+        Gets the length in bytes for each piece of download
+        """
+        return self.meta_info[b'info'][b'piece length']
+    
+    @property
+    def total_size(self) -> int:
+        """
+        :return: The total size (in bytes) for this torrent's data.
+        """
+        if self.multi_file:
+            raise RuntimeError('Multi-file torrents are not supported!')
+        return self.files[0].length
+    
+    @property
+    def pieces(self) -> list[str]:
+        """
+        Breaks the string meta_info pieces into 20 byte long slices. 
+        20 bytes being 20 characters in a string representation
+        """
+        data = self.meta_info[b'info'][b'pieces']
+        pieces = []
+        offset = 0
+        length = len(data)
 
-            while offset < length:
-                pieces.append(data[offset:offset + 20])
-                offset += 20
-            return pieces 
+        while offset < length:
+            pieces.append(data[offset:offset + 20])
+            offset += 20
+        return pieces 
 
-        @property 
-        def output_file(self):
-            return self.meta_info[b'info'][b'name'].decode('utf-8')
-        
-        def __str__(self):
-            return (f"Filename: {self.meta_info[b'info'][b'name'].decode('utf-8')}\n"
-                    f"File length: {self.meta_info[b'info'][b'length']}\n"
-                    f"Announce URL: {self.meta_info[b'announce'].decode('utf-8')}\n"
-                    f"Hash: {self.info_hash.hex()}")
+    @property 
+    def output_file(self):
+        return self.meta_info[b'info'][b'name'].decode('utf-8')
+    
+    def __str__(self):
+        return (f"Filename: {self.meta_info[b'info'][b'name'].decode('utf-8')}\n"
+                f"File length: {self.meta_info[b'info'][b'length']}\n"
+                f"Announce URL: {self.meta_info[b'announce'].decode('utf-8')}\n"
+                f"Hash: {self.info_hash.hex()}")
