a128e1a1bbb59a350053b68b96368c2c2793baca
Bernard <bernardmonagan@gmail.com>
2025-10-02 20:58:29 -0400

    Changed main function to an async to properly use work with python standards up to 3.12+

--- src/main.py
+++ src/main.py
@@ -8,19 +8,16 @@ from .torrent import Torrent
 from .client import TorrentClient
 
 
-def main():
+async def async_main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('torrent',
-                        help='the .torrent to download')
-    parser.add_argument("-v","--verbose",action='store_true',
-                        help ='enable verbose output')
+    parser.add_argument('torrent', help='the .torrent to download')
+    parser.add_argument("-v","--verbose",action='store_true', help ='enable verbose output')
     args = parser.parse_args()
     if args.verbose:
         logging.basicConfig(level=logging.INFO)
 
-    loop = asyncio.get_event_loop()
     client = TorrentClient(Torrent(args.torrent))
-    task = loop.create_task(client.start())
+    task = asyncio.create_task(client.start())
 
     def signal_handler(*_):
         logging.info('Exiting, please wait untill everything is shutdown...')
@@ -30,9 +27,10 @@ def main():
     signal.signal(signal.SIGINT, signal_handler)
 
     try:
-        loop.run_until_complete(task)
+        await task
     except CancelledError:
         logging.warning('Event loop was canceled.')
 
+
 if __name__ == "__main__":
-    main()
+    asyncio.run(async_main())
